f05fdd5932f12cd5ccb49785529ae50da5c6919b
doraniii <https://github.com/doraniii>
2024-10-04 17:13:27 +0900

    24_10_04

--- Data_Crawling.py
+++ Data_Crawling.py
@@ -7,23 +7,27 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver import ActionChains
 
 options = Options()
 options.add_argument("--headless")
+options.add_argument("start-maximized")
+options.add_argument('--disable-dev-shm-usage')
 # 리눅스 서버같은 GUI환경이 지원안될 때 필요한 옵션
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
+# 디버깅 모드를 위한 옵션
+options.add_argument('--remote-debugging-port=8903')
 
 url = 'https://whiskyadvocate.com/ratings-reviews'
 driver = webdriver.Chrome(options=options)
 
-# 위스키 스타일을 선택. select box로 선택할 수 있는 모든 위스키 스타일을 list로 반환
-def func_selectStyle(url) :
-    urlList = []
-    driver.get(url)
+eleClass = 'mwInput.checkList.new.filter.select.multiline.noMobile.name-filtersdefaultcustom_rating_category.list'
+subBtnEle = 'submit.button.mwFilter-submit.mwFormSubmit'
 
-    getElements = driver.find_element(By.CLASS_NAME, 'mwInput.checkList.new.filter.select.multiline.noMobile.name-filtersdefaultcustom_rating_category.list')
+# 필요한 Elements 가져오기
+def func_findEle(eleClass) :
+    getElements = driver.find_element(By.CLASS_NAME, eleClass)
 
     # 리스트 가져오기
     getList = getElements.find_element(By.CLASS_NAME, 'list')
@@ -34,25 +38,42 @@ def func_selectStyle(url) :
     select = Select(select_element)
     option_list = select.options
 
-    # 버튼
-    searchBtn = driver.find_element(By.CLASS_NAME,'submit.button.mwFilter-submit.mwFormSubmit')
-    resetBtn = driver.find_element(By.CLASS_NAME, 'reset.button.mwFilter-reset.mwFormSubmit')
+    return child_divs, option_list
 
-    for list, opt in zip(child_divs[1:3], option_list[1:3]) :
-   
+# 위스키 스타일을 선택. 선택한 option별로 url이 달라지기 때문에 옵션 선택 별로 url 저장
+def func_selectStyle(url, eleClass, subBtnEle) :
+    urlList = []
+    driver.get(url)
+
+    child_divs, option_list = func_findEle(eleClass)
+
+    for i in range(1, len(child_divs)) :
+        list = child_divs[i+1]
+        opt = option_list[i+1]
+        action = ActionChains(driver)
+        action.move_to_element(driver.find_element(By.CLASS_NAME, eleClass)).perform()
+
         driver.execute_script("arguments[0].classList.add('selected');", list)
         driver.execute_script("arguments[0].setAttribute('selected', 'selected');", opt)
 
+        action.move_to_element(driver.find_element(By.CLASS_NAME, subBtnEle)).perform()
+        searchBtn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, subBtnEle)))
+
         searchBtn.click()
+
         time.sleep(5)
         currentUrl = driver.current_url
-        print(currentUrl)
         urlList.append(currentUrl)
-        time.sleep(10)
-        driver.execute_script("arguments[1].classList.remove('selected');", list)
-        driver.execute_script("arguments[1].removeAttribute('selected');", opt)
 
- 
+        time.sleep(5)
+        action.move_to_element(driver.find_element(By.CLASS_NAME, eleClass)).perform()
+
+        resetBtn = driver.find_element(By.CLASS_NAME, 'reset.button.mwFilter-reset.mwFormSubmit')
+        resetBtn.click()
+        time.sleep(5)
+        
+        child_divs, option_list = func_findEle(eleClass)
+
     return urlList
 
-func_selectStyle(url)
+optionUrlList = func_selectStyle(url, eleClass, subBtnEle)
