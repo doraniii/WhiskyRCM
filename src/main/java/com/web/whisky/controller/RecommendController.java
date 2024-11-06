814f7c6de23675406b59b8b2779cb8cd1ba83a27
doraniii <qwas6435@gmail.com>
2024-11-06 20:36:06 +0900

    241106 weaviateDB 커넥션 테스트 중

--- src/main/java/com/web/whisky/controller/RecommendController.java
+++ src/main/java/com/web/whisky/controller/RecommendController.java
@@ -1,14 +1,56 @@
 package com.web.whisky.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import io.weaviate.client.Config;
+import io.weaviate.client.WeaviateClient;
+import io.weaviate.client.base.Result;
+import io.weaviate.client.v1.graphql.model.GraphQLResponse;
+import io.weaviate.client.v1.graphql.query.argument.NearTextArgument;
+import io.weaviate.client.v1.graphql.query.fields.Field;
+import io.weaviate.client.v1.misc.model.Meta;
 
 @Controller
 public class RecommendController {
 
+	/* 메인화면 */
 	@GetMapping("/")
-	public String test() {
+	public String mainPage() {
 		return "MainQuery";
 	}
 
+	/* 쿼리를 받아와서 WeaviateDB에 던지기*/
+	@PostMapping("/main/commitQuery")
+	public String commitQuery(Map<String, Object> params) {
+				
+		Config config = new Config("http", "172.19.0.4:8080");
+		WeaviateClient client = new WeaviateClient(config);	
+		
+		NearTextArgument nearText = client.graphQL().arguments().nearTextArgBuilder()
+			      .concepts(new String[]{ (String) params.get("query") })
+			      .distance(0.6f) // use .certainty(0.7f) prior to v1.14
+			      .build();
+		
+		Field name = Field.builder().name("name").build();
+	    Field _additional = Field.builder()
+	      .name("_additional")
+	      .fields(new Field[]{
+	        Field.builder().name("certainty").build(), // only supported if distance==cosine
+	        Field.builder().name("distance").build(),  // always supported
+	      }).build();
+	    
+		Result<GraphQLResponse> result = client.graphQL().get()
+			      .withClassName("whisky")
+			      .withFields(name, _additional)
+			      .withNearText(nearText)
+			      .run();
+		
+		return result.toString();
+	}
+	
 }
--- src/main/webapp/WEB-INF/views/MainQuery.jsp
+++ src/main/webapp/WEB-INF/views/MainQuery.jsp
@@ -8,14 +8,41 @@
 </head>
 <body>	
 	<div>
-		<input type="text" placeholder="Please tell me your taste in whiskey.">
-		<button>commit</button>
+		<form name="whiskyInfo" id="whiskyInfo" method="post">
+			<input type="text" name="query" placeholder="Please tell me your taste in whiskey.">
+			<button type="button" id="commit">commit</button>
+		</form>
 	</div>
-	
+
+<script src="/lib/jquery-3.7.1.min.js"></script>
 <script type="text/javascript">
-
-//function
-
+$(document).ready(function() {
+	
+	set_Event();
+	
+	function set_Event() {
+		$("#commit").click(function() {
+			var query = $("input[name=query]").val();
+			func_runQuery(query);
+		});
+	}
+	
+	function func_runQuery(query) {
+		var params = {};
+		params['query'] = query;
+		console.log(params)
+		
+		$.ajax({
+			url : "/main/commitQuery",
+			type : "POST",			
+			data : JSON.stringify(params),
+			//요청 성공 시 동작할 콜백 함수 지정
+			success : function(data) {
+				console.log(data);
+			}
+		});
+	}
+})
 </script>
 </body>
 </html>
