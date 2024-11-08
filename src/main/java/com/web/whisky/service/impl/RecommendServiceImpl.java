567e552794166e005eb22d3954c665c7be1eb567
doraniii <qwas6435@gmail.com>
2024-11-08 23:52:16 +0900

    241108 DB 커넥션 후 Response 값 확인

--- src/main/java/com/web/whisky/service/impl/RecommendServiceImpl.java
+++ src/main/java/com/web/whisky/service/impl/RecommendServiceImpl.java
@@ -1,11 +1,15 @@
 package com.web.whisky.service.impl;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.web.whisky.service.RecommendService;
 
 import io.weaviate.client.Config;
@@ -13,7 +17,9 @@ import io.weaviate.client.WeaviateClient;
 import io.weaviate.client.base.Result;
 import io.weaviate.client.v1.graphql.model.GraphQLResponse;
 import io.weaviate.client.v1.graphql.query.argument.NearTextArgument;
+import io.weaviate.client.v1.graphql.query.builder.GetBuilder;
 import io.weaviate.client.v1.graphql.query.fields.Field;
+import io.weaviate.client.v1.graphql.query.fields.Fields;
 
 @Service
 public class RecommendServiceImpl implements RecommendService {
@@ -30,28 +36,48 @@ public class RecommendServiceImpl implements RecommendService {
 		Config config = new Config("http", host + ":" + port);
 		WeaviateClient client = new WeaviateClient(config);	
 		
-		NearTextArgument nearText = client.graphQL().arguments().nearTextArgBuilder()
-			      .concepts(new String[]{ (String) params.get("query") })
-			      .distance(0.6f) // use .certainty(0.7f) prior to v1.14
-			      .build();
+		String[] target_vector = {"review_vector"};
 		
-		Field name = Field.builder().name("name").build();
-	    Field _additional = Field.builder()
-	      .name("_additional")
-	      .fields(new Field[]{
-	        Field.builder().name("certainty").build(), // only supported if distance==cosine
-	        Field.builder().name("distance").build(),  // always supported
-	      }).build();
-	    
-		Result<GraphQLResponse> result = client.graphQL().get()
-			      .withClassName("whisky")
-			      .withFields(name, _additional)
-			      .withNearText(nearText)
-			      .run();
+		NearTextArgument nearText = NearTextArgument.builder()
+		    .concepts(new String[]{ (String) params.get("query") })
+			.targetVectors(target_vector)
+			.build();
+
+		Fields fields = Fields.builder()
+			.fields(new Field[]{
+			Field.builder().name("name").build(),
+			Field.builder().name("category").build(),
+			Field.builder().name("review").build(),
+			}).build();
+
+		String query = GetBuilder.builder()
+		    .className("Whisky")
+		    .fields(fields)
+			.withNearTextFilter(nearText)
+			.limit(2)
+			.build()
+			.buildQuery();
+
+		Result<GraphQLResponse> result = client.graphQL().raw().withQuery(query).run();
+        
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            JsonNode rootNode = objectMapper.valueToTree(result.getResult());
+
+            JsonNode whiskyNode = rootNode.at("/data/Get/Whisky");
+            
+            String data = whiskyNode.toString();
+            data = data.replaceAll("%", " percentage");
+            
+		return data;
+	}
+
+	
+	@Override
+	public String translationReview(Map<String, Object> params) {
 		
-		
-		
-		return result.getResult().toString();
+		/* Reponse 받은 결과를 Naver API를 써서 번역 */
+		return null;
 	}
 
 }
